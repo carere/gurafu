78b80d460cd9b6526663ec7b1dedb040dc63f980
Kevin Abatan <abatan.k@gmail.com>
2025-04-27 19:21:45 +0200

    chore: add tailwind, solid-ui & font-awesome

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solid()],
-})
+  plugins: [solid(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
