4c49dec7b02e8a427b17287232aace73a10c29e2
Kevin Abatan <abatan.k@gmail.com>
2025-06-05 11:51:58 +0200

    chore: adding some configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,10 @@
 import { createSignal } from "solid-js";
-import viteLogo from "/vite.svg";
-import solidLogo from "./assets/solid.svg";
-import "./globals.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [count, setCount] = createSignal(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank" rel="noreferrer">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">Click on the Vite and Solid logos to learn more</p>
-    </>
-  );
+  return <Button onClick={() => setCount(count() + 1)}>Clicked {count()} times</Button>;
 }
 
 export default App;
